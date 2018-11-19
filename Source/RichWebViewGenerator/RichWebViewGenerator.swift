a76aab2083dae12b526e82e137cb5f5cbbd75daa
Ahmed Elkady <ahmed.elkady@tophatmonocle.com>
2018-11-19 14:17:21 -0500

    Generate webviews

--- Source/RichWebViewGenerator/RichWebViewGenerator.swift
+++ Source/RichWebViewGenerator/RichWebViewGenerator.swift
@@ -6,6 +6,36 @@
 //  Copyright © 2018 Top Hat. All rights reserved.
 //
 
+import WebKit
+
 class RichWebViewGenerator {
 
+    // MARK: - Init
+
+    private init() {}
+
+    // MARK: - Utility Functions
+
+    static func getWebView(from input: String) -> WKWebView? {
+        guard let url = RichWebViewGenerator.getWebViewURL(from: input) else {
+            return nil
+        }
+        let configuration = WKWebViewConfiguration()
+        configuration.allowsInlineMediaPlayback = true
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        webView.load(URLRequest(url: url))
+        return webView
+    }
+
+    // MARK: - Private Helpers
+
+    private static func getWebViewURL(from input: String) -> URL? {
+        if let youTubeID = input.getSubstring(inBetween: "youtube[", and: "]") {
+            return URL(string: "https://www.youtube.com/embed/" + youTubeID + "?playsinline=1")
+        }
+        else if let vimeoID = input.getSubstring(inBetween: "vimeo[", and: "]") {
+            return URL(string: "https://player.vimeo.com/video/" + vimeoID + "?title=0&byline=0&portrait=0")
+        }
+        return nil
+    }
 }
