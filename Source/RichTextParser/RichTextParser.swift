6dcc028444e1913949fe1ee8cce09f8bc64192db
OrlaM <mitchell.orla@gmail.com>
2018-11-20 11:29:06 -0500

    Issue #12 tie parsing methods together (#33)
    * Added latex parsing
    
    * Removed redundant code and fixed tests
    
    * Fixed edge case
    
    * Merge from master
    
    * pr comments

--- Source/RichTextParser/RichTextParser.swift
+++ Source/RichTextParser/RichTextParser.swift
@@ -6,8 +6,13 @@
 //  Copyright © 2018 Top Hat. All rights reserved.
 //
 
+import Down
+
 class RichTextParser {
 
+    private enum ParserConstants {
+        static let latexRegex = "\\[math\\](.*?)\\[\\/math\\]"
+    }
     // MARK: - Dependencies
 
     let latexParser: LatexParserProtocol
@@ -18,19 +23,58 @@ class RichTextParser {
         self.latexParser = latexParser
     }
 
+    func richTextToAttributedString(from input: String) -> NSAttributedString {
+        let components = self.seperateComponents(from: input)
+        let attributedArray = self.generateAttributedStringArray(from: components)
+        let mutableAttributedString = NSMutableAttributedString()
+        for attributedString in attributedArray {
+            mutableAttributedString.append(attributedString)
+        }
+        return mutableAttributedString
+    }
+
     // MARK: - Helpers
+
+    func generateAttributedStringArray(from input: [String]) -> [NSAttributedString] {
+        var output = [NSAttributedString]()
+        for element in input {
+            if let attributedString = self.getAttributedText(from: element) {
+                output.append(attributedString)
+            } else {
+                // TODO: #34 Add error handling
+            }
+        }
+        return output
+    }
+
+    private func getAttributedText(from input: String) -> NSAttributedString? {
+        if isTextLatex(input) {
+            return self.extractLatex(from: input)
+        }
+        return try? Down(markdownString: input).toAttributedString()
+    }
+
+    func seperateComponents(from input: String) -> [String] {
+        let splitPositions = self.extractPositions(fromRanges: input.ranges(of: ParserConstants.latexRegex, options: .regularExpression))
+        if splitPositions.count == 0 {
+            return [input]
+        }
+        return input.split(
+            atPositions: splitPositions
+        )
+    }
 
     func extractLatex(from input: String) -> NSAttributedString {
         return self.latexParser.extractLatex(from: input)
     }
 
-    func isTextHTML(_ text: String) -> Bool {
-        do {
-            let regexPattern = "(?:</[^<]+>)|(?:<[^<]+/>)"
-            let regex = try NSRegularExpression(pattern: regexPattern, options: .caseInsensitive)
-            return regex.numberOfMatches(in: text, options: [], range: NSMakeRange(0, text.count)) != 0
-        } catch {
-            return false
-        }
+    func isTextLatex(_ text: String) -> Bool {
+        return text.ranges(of: ParserConstants.latexRegex, options: .regularExpression).count != 0
+    }
+    
+    private func extractPositions(fromRanges ranges: [Range<String.Index>]) -> [String.Index] {
+        return ranges.flatMap { range in
+            return [range.lowerBound, range.upperBound]
+        }.sorted()
     }
 }
--- UnitTests/RIchTextParser/RichTextParserSpec.swift
+++ UnitTests/RIchTextParser/RichTextParserSpec.swift
@@ -16,11 +16,11 @@ class RichTextParserSpec: QuickSpec {
         static let regularText = "Some Text"
         static let regularTextWithFalseFlags = "Some \"Text\" with random >p and some <no closing tag>"
         static let standardHTML = "<p>some text</p>"
-        static let complexHTML = "<p><div><randomtext>M</randomtext></div></p>"
+        static let complexHTML = "<p><div><randomtext>Message</randomtext></div></p>"
         static let basicMarkdown = "**More Text**"
-        static let complextMarkdown = "#text **something *more words* ~(testing brackets)~"
+        static let complexMarkdown = "#text **something *more words* ~(testing brackets)~"
         static let basicLatex = "[math]x^n[/math]"
-        static let complextLatext = "[math]x =\\dfrac{\\dfrac{a}{b}}{c} = \\dfrac{\\frac{\\textstyle a}{\\textstyle b}}{c} = \\dfrac{\\frac{a}{b}}{c} [/math]"
+        static let complexLatex = "[math]x^2[/math] **More Text** [math]x^n+5=2[/math]"
     }
 
     var richTextParser: RichTextParser!
@@ -30,38 +30,48 @@ class RichTextParserSpec: QuickSpec {
             beforeEach {
                 self.richTextParser = RichTextParser()
             }
-            context("HTML parsing") {
-                it("successfully identifies various HTML text") {
-                    expect(self.richTextParser.isTextHTML(MarkDownText.standardHTML)).to(beTrue())
-                    expect(self.richTextParser.isTextHTML(MarkDownText.complexHTML)).to(beTrue())
-                }
-                it("succssfully rejects non-markdown text") {
-                    expect(self.richTextParser.isTextHTML(MarkDownText.regularText)).to(beFalse())
-                    expect(self.richTextParser.isTextHTML(MarkDownText.regularTextWithFalseFlags)).to(beFalse())
-                }
-                it("successfully rejects markdown text") {
-                    expect(self.richTextParser.isTextHTML(MarkDownText.basicMarkdown)).to(beFalse())
-                    expect(self.richTextParser.isTextHTML(MarkDownText.complextMarkdown)).to(beFalse())
-                }
-                it("successfully rejects latex text") {
-                    expect(self.richTextParser.isTextHTML(MarkDownText.basicLatex)).to(beFalse())
-                    expect(self.richTextParser.isTextHTML(MarkDownText.complextLatext)).to(beFalse())
-                }
-            }
             context("Latex Parsing") {
                 it("succesfully returns an NSAttributedString with an image") {
                     let output = self.richTextParser.extractLatex(from: MarkDownText.basicLatex)
-                    expect(output).to(beAKindOf(NSAttributedString.self))
-                    var hasImage = false
-                    let fullRange = NSRange(location: 0, length: output.length)
-                    output.enumerateAttribute(.attachment, in: fullRange, options: []) { (value, _, _) in
-                        guard let attachment = value as? NSTextAttachment else { return }
+                    self.testAttributedStringContainsImage(output)
+                }
+            }
+            context("Breaking up text into componenets") {
+                it("seperates latex from html/markdown and maintains the order") {
+                    let components = self.richTextParser.seperateComponents(from: MarkDownText.complexLatex)
 
-                        if let _ = attachment.image {
-                            hasImage = true
-                        }
-                    }
-                    expect(hasImage).to(beTrue())
+                    expect(components.count).to(equal(3))
+                    expect(components[0]).to(equal("[math]x^2[/math]"))
+                    expect(components[1]).to(equal(" **More Text** "))
+                    expect(components[2]).to(equal("[math]x^n+5=2[/math]"))
+                }
+                it("generates an attributed string array with the correct components for latex and markdown") {
+                    let components = self.richTextParser.seperateComponents(from: MarkDownText.complexLatex)
+                    let attributedStrings = self.richTextParser.generateAttributedStringArray(from: components)
+
+                    expect(attributedStrings.count).to(equal(3))
+                    expect(attributedStrings[1].string).to(equal("More Text\n"))
+                    self.testAttributedStringContainsImage(attributedStrings[0])
+                    self.testAttributedStringContainsImage(attributedStrings[2])
+                }
+                it("generates an attributed string array with the correct components for html") {
+                    let components = self.richTextParser.seperateComponents(from: MarkDownText.complexHTML)
+                    let attributedStrings = self.richTextParser.generateAttributedStringArray(from: components)
+
+                    expect(attributedStrings.count).to(equal(1))
+                    expect(attributedStrings[0].string).to(equal("\nMessage\n\n"))
+                }
+            }
+            context("Rich text to attributed string") {
+                it("generates a single attributed string with multiple rich text types") {
+                    let regularText = self.richTextParser.richTextToAttributedString(from: MarkDownText.regularText)
+                    expect(regularText.string.range(of: "Some Text")).toNot(beNil())
+
+                    let complexHTML = self.richTextParser.richTextToAttributedString(from: MarkDownText.complexHTML)
+                    expect(complexHTML.string.range(of: "Message")).toNot(beNil())
+
+                    let complexLatex = self.richTextParser.richTextToAttributedString(from: MarkDownText.complexLatex)
+                    expect(complexLatex.string.range(of: "More Text")).toNot(beNil())
                 }
             }
             context("Memory leaks") {
@@ -86,5 +96,19 @@ class RichTextParserSpec: QuickSpec {
                 }
             }
         }
+    }
+
+    private func testAttributedStringContainsImage(_ input: NSAttributedString) {
+        expect(input).to(beAKindOf(NSAttributedString.self))
+        var hasImage = false
+        let fullRange = NSRange(location: 0, length: input.length)
+        input.enumerateAttribute(.attachment, in: fullRange, options: []) { (value, _, _) in
+            guard let attachment = value as? NSTextAttachment else { return }
+
+            if let _ = attachment.image {
+                hasImage = true
+            }
+        }
+        expect(hasImage).to(beTrue())
     }
 }
