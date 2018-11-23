0883fe9003b4aab05b9197b33ee93833c24114ae
Ahmed Elkady <ahmed.elkady@tophatmonocle.com>
2018-11-23 16:57:25 -0500

    Update podspec version bumping

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-sed "s/VERSION_REPLACED_BY_CI/${CIRCLE_TAG}/g" THRichTextView.podspec > THRichTextView.podspec.tmp
-cat THRichTextView.podspec.tmp
-mv THRichTextView.podspec.tmp THRichTextView.podspec
+npm install -g podspec-bump
+podspec-bump -w
