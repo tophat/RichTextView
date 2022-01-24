f85bc9186460141191ffc49636c2ea4d08430567
Blair McArthur <blair.mcarthur@tophatmonocle.com>
2022-01-24 10:08:11 -0500

    Created a Package.swift

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "RichTextView",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(name: "RichTextView", targets: ["RichTextView"])
+    ],
+    dependencies: [
+        .package(name: "Down", url: "https://github.com/johnxnguyen/Down", .upToNextMajor(from: "0.11.0")),
+        .package(name: "SnapKit", url: "https://github.com/SnapKit/SnapKit", .upToNextMajor(from: "5.0.1")),
+        .package(name: "iosMath", url: "https://github.com/tophatmonocle/iosMath", .branch("mathfonts-resource-bundle")),
+    ],
+    targets: [
+        .target(
+            name: "RichTextView",
+            dependencies: [
+                .product(name: "Down", package: "Down"),
+                .product(name: "SnapKit", package: "SnapKit"),
+                .product(name: "iosMath", package: "iosMath"),
+            ],
+            path: "Source",
+            exclude: ["Info.plist"]
+        ),
+    ]
+)
