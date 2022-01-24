0693b036af9a84601a87de3340382b318d39fd21
Blair McArthur <blair.mcarthur@tophatmonocle.com>
2022-01-24 14:36:53 -0500

    Switching to master branch of our iosMath fork

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         .package(name: "Down", url: "https://github.com/johnxnguyen/Down", .upToNextMajor(from: "0.11.0")),
         .package(name: "SnapKit", url: "https://github.com/SnapKit/SnapKit", .upToNextMajor(from: "5.0.1")),
-        .package(name: "iosMath", url: "https://github.com/tophatmonocle/iosMath", .branch("mathfonts-resource-bundle")),
+        .package(name: "iosMath", url: "https://github.com/tophatmonocle/iosMath", .branch("master")),
     ],
     targets: [
         .target(
