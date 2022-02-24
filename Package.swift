94c61b114737e99e75fecae5d3f4000d2185bd5c
maicon-brauwers <maicon.brauwers@tophatmonocle.com>
2022-02-24 10:01:12 -0500

    pointing to SwiftRichString branch

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .package(name: "Down", url: "https://github.com/johnxnguyen/Down", .upToNextMajor(from: "0.11.0")),
         .package(name: "SnapKit", url: "https://github.com/SnapKit/SnapKit", .upToNextMajor(from: "5.0.1")),
         .package(name: "iosMath", url: "https://github.com/tophatmonocle/iosMath", .upToNextMajor(from: "1.1.1")),
-        .package(name: "SwiftRichString", url: "https://github.com/malcommac/SwiftRichString.git", .upToNextMajor(from: "3.7.2"))
+        .package(name: "SwiftRichString", url: "https://github.com/tophatmonocle/SwiftRichString", .branch("fix-parent-child-font-styles"))
     ],
     targets: [
         .target(
